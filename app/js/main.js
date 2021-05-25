dec22703c000208c3b6c23c069cd27d6262b33e9
DarkGoanna <darkgoanna@gmail.com>
2021-05-25 11:13:55 +0300

    finish

--- app/js/main.js
+++ app/js/main.js
@@ -1,15 +1,12 @@
 // sticky header
-if (window.matchMedia("(min-width: 768px)").matches) {
-  const header = document.querySelector('.header');
-
-  document.addEventListener('scroll', () => {
-      if (window.scrollY > 50) {
-          header.classList.add('background');
-      } else {
-          header.classList.remove('background');
-      }
-  })
-}
+const header = document.querySelector('.header');
+document.addEventListener('scroll', () => {
+    if (window.scrollY > 50) {
+        header.classList.add('background');
+    } else {
+        header.classList.remove('background');
+    }
+})
 
 // menu
 const burger = document.querySelector('.menu__burger');
@@ -20,15 +17,6 @@ burger.addEventListener('click', () => {
 
     const menuOpen = document.querySelector('.menu.open');
     const menu = document.querySelector('.menu');
-
-    if (menuOpen) {
-        // добавить событие открытия меню
-        menuOpen.addEventListener('click', openSubmenu);
-    } else {
-        // убрать событие если закрыто 
-        menu.removeEventListener('click', openSubmenu);
-    }
-
 })
 
 // our products
@@ -67,3 +55,21 @@ $('.promotions__inner').slick({
     prevArrow:'<button class="promotions__arrow promotions__arrow_left"></button>',
     nextArrow:'<button class="promotions__arrow promotions__arrow_right"></button>',
 });
+
+// popup
+const popup = document.querySelector('.popup-wrapper');
+document.querySelectorAll('.call-popup').forEach(btn => {
+  btn.addEventListener('click', () => {
+    popup.classList.add('open');
+    window.classList.add('scrollOff');
+  })
+})
+popup.addEventListener('click', e => {
+  if (e.target.classList.contains('popup-wrapper') || e.target.classList.contains('popup__close')) {
+      popup.classList.remove('open');
+      window.classList.remove('scrollOff');
+  }
+})
+
+// aos
+AOS.init();
